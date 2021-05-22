846c2315bf0ab52264c45c013893dc6c3c236ba1
Wipu <wipugeng@hotmail.com>
2021-05-22 16:58:40 +0700

    further clean up #2

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,12 +3,7 @@ package application;
 
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import logic.SceneManager;
 
